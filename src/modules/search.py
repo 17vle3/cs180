a386eb20edd65d42fde8ac24e41c2a9b1d18632a
unknown <17vle3@gmail.com>
2020-10-29 12:42:28 -0700

    search is by substring not ==

--- src/modules/search.py
+++ src/modules/search.py
@@ -1,14 +1,21 @@
 #Takes in the projectList (list of project dictionaries). Returns either one project(unique ID) or a list of matches for the search term
 def search(searchTerm, searchFilter, projectList):
-    if searchFilter == 'id':
+    if searchFilter == 'id': #for ID searches, there can only be one, so
         for project in projectList:
             if project[searchFilter] == searchTerm:
                 matches = []
                 matches.append(project)
                 return matches
-
-    else:
-        matches = list(filter(lambda project: project[searchFilter].lower() == searchTerm.lower(), projectList))
-        if not matches:
-            return None
+    else :
+        matches = []
+        for project in projectList:
+            if searchTerm.lower() in project[searchFilter].lower():
+                matches.append(project)
         return matches
+    #else: #for other types of searches
+
+    #    matches = list(filter(lambda project: project[searchFilter].lower().find(searchTerm.lower()) != -1 , projectList))
+        #the results HAVE to equal the search term. 
+    #    if not matches:
+    #        return None
+    #    return matches
